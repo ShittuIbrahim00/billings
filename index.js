9f03d70d23064ee128083d17e9bddfface3512f0
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-03-27 11:23:18 +0100

    testing CORS

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(cookieParser());
 connectDB();
 
-const corOption = {
+const corOptions = {
   origin: process.env.CLIENT_URL || ["http://localhost:5173"],
   credentials : true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
-app.use(cors(corOption));
+app.use(cors(corOptions));
 app.use("/api/v1", authRouter);
 
 const PORT = process.env.PORT || 5000;
